3362275d93d15bb2e1499cede35366f1bf63dd12
FendShooter <virgile.dokouvi@gmail.com>
2021-05-12 19:50:11 -0400

    posrtasa

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(morgan('dev'));
 // routers
 app.use('/api/v1', require('./routers/routes'));
-
+ 
 const PORT = process.env.PORT || 5000;
 db.sequelize
   // .sync({ force: true })
--- models/index.js
+++ models/index.js
@@ -4,16 +4,14 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-  // sequelize = new Sequelize(
-  //   'postgres://wasrjnvfbmpvyh:4b2f8e647868ad78f2797ec84f882436c8e0579e66c60473d864ceddd587be19@ec2-52-21-153-207.compute-1.amazonaws.com:5432/dce3q3fa33o8kn'
-  // );
+
 } else {
   sequelize = new Sequelize(
     config.database,
